0c5bea4c6473bcb704ffedb4d3db21a943c35dba
chibi11-043 <101236247+chibi11-043@users.noreply.github.com>
2022-04-05 15:07:34 +0700

    Labwork 2

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,3 +1,6 @@
+#include "StudentInformation"
+#include "CourseInformation"
+
 studentList=[]
 from itertools import count
 #1. input number of students in a class
@@ -6,23 +9,19 @@ def CountStudents():
 	return count
 
 #2. input student information
-#4. input course information
 class Student:
     count = 0
     pass
     # initializer/object attributes
-    def __init__(self,id,name,dob,courseID,courseName,mark):
+    def __init__(self,id,name,dob):
         print("Enter your Student Information :")
-        self.id = id
-        self.name = name 
-        self.dob = dob
+        self.id=id  
+        self.name=name
+        self.dob=dob
         Student.count +=1
-        #self.courseID = courseID
-        #self.courseName = courseName 
-        #self.mark = mark
+        A = {'id' : id, 'name' : name, 'dob' : dob}
+        Student.append(A)
         
-    #def get_id(self,id)
-    #    self.id=id
     #id
     def get_id(self):
         return self.id
@@ -36,6 +35,23 @@ class Student:
         self.dob = dob
     def get_dob(self):
         return self.dob
+    
+#3. input number of courses :
+def NumberOfCourses() :
+    NumberOfCourses = int(input("Enter number of courses : "))
+    return NumberOfCourses
+
+#4. input courses information :
+class Course :
+    count = 0
+    pass
+    def __init__(self,courseID,courseName,mark):
+        self. courseID=courseID 
+        self.courseName=courseName
+        self.mark=mark
+        Course.count +=1
+        B = {"id":courseID,"name":courseName,"mark":mark}
+        Course.append(B)
     #courseID
     def set_courseID(self,courseID):
         self.courseID = courseID
@@ -51,22 +67,41 @@ class Student:
         self.mark = mark
     def get_mark(self):
         return self.mark
-    id=input(f"Enter the student ID :")  
-    name=input(f"Enter the student name :")
-    dob=input(f"Enter the student dob :")
-    print(f"Enter the Course Information :")
-    courseID=input(f"Enter the Course ID :") 
-    courseName=input(f"Enter the Course Name :")
-    mark=input(f"Enter the mark :") 
 
-    #main
-    sv = Student(id,name,dob,courseID,courseName,mark)
-    studentList.append(sv)
+#5.Select a course , input marks for student in this course
 
+        
 
-#3. input number of courses :
-def NumberOfCourses() :
-    NumberOfCourses = int(input("Enter number of courses : "))
-    return NumberOfCourses
+class test_oop:
+    Students = []
+    Courses = []
+    
+    def listStudent (self) :
+        print(Student)
 
+    def listCourse (self) :
+        print(Course)
+    
+    
+    def start (self):  
+        A = int(input('How many student are there in the course : '))
+        for i in range (A) :
+            self.id=input(f"Enter the student ID :")  
+            self.name=input(f"Enter the student name :")
+            self.dob=input(f"Enter the student dob :")
+            temp_Student = Student(self.id,self.name,self.dob)
+            
+            
+        B = int(input('Enter number of courses :'))
+        for y in range (B):
+            courseID = input("Enter the course id : ")
+            courseName = input("Enter the course name : ")
+            mark = input("Enter the mark of course :")
+            temp_Course = Course(self,courseID,courseName,mark)
+        
+    
 
+if __name__ == '__main__':
+    t = test_oop()
+    t.start()
+    
